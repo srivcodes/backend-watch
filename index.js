409e3cbd3d85cdfd75134d0ae7b60591236ae7c9
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-02 18:56:53 +0530

    added headers bypass

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
 dbConnection();
 
